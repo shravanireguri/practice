49d403a22f7a56702955204162035ad81c2534a2
Shravani Reguri <shravanireguri99@gmail.com>
2024-03-19 14:50:07 +0530

    submitting the code.

---
+++ src/main/java/Addition.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Addition {
+	public void add() {
+		int num1 = 10;
+		int num2 = 20;
+
+		int res = num1 + num2;
+
+		System.out.println(res);
+	}
+}
+
+
